85fd61bdd3405813bec3fbbda93ec5b67dc8745f
PiyumalNipuna60 <nipuna315np@gmail.com>
2025-03-24 16:19:23 +0530

    added location details to email template and remove other logged in devices

--- resources/views/email/footer.blade.php
+++ resources/views/email/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="footer">
     <div style="    margin-top: 15px;">Thanks!<br>TicketBooth.lk Team</div>
     <hr style="border: 0; border-top: 1px solid #ddd; margin: 20px 0;">
-    <div style="font-size: 11px; color: #aaaaaa !important; line-height: 16px;">
+    <div style="font-size: 11px; color: #aaaaaa !important; line-height: 16px; text-align: center;">
         <div>from</div>
         <img src="https://zoombus-web.s3.eu-north-1.amazonaws.com/logo.png" alt="TicketBooth.lk Logo" style="max-width: 150px; height: auto; margin-bottom: 10px;">
         <div>© TicketBooth.lk, Tisara Plaza, Thompson Associates Pvt Ltd, No.127/88,Level 5, Colombo 00600</div>
